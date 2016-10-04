8db1f87329cf7187f07f56f658baa30e9aed397d
Peter Zhen <peterzhen0713@gmail.com>
2016-10-04 11:09:12 -0400

    setup backend users and session

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,49 @@
 #
 
 class User < ActiveRecord::Base
+  attr_reader :password
+
+  after_initialize :ensure_session_token
+
+  validates :name, :password_digest, :session_token, presence: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+  validates :session_token, :name, uniqueness: true
+
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by_username(username)
+    user.try(:is_password?, password) ? user : nil
+  end
+
+  def self.generate_session_token
+    token = SecureRandom::urlsafe_base64(16)
+    while self.session_token == token
+      token = SecureRandom::urlsafe_base64(16)
+    end
+    token
+  end
+
+  def is_password?(unencrypted_password)
+    BCrypt::Password.new(self.password_digest).is_password?(unencrypted_password)
+  end
+
+  def password=(unencrypted_password)
+    if unencrypted_password.present?
+      @password = unencrypted_password
+      self.password_digest = BCrypt::Password.create(unencrypted_password)
+    end
+  end
+
+  def reset_session_token!
+    self.session_token = self.class.generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  private
+
+  def ensure_session_token
+    self.session_token ||= self.class.generate_session_token
+  end
+
 end
