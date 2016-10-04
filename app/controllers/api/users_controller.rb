c8018e2e8095a93096d396818b2918cc8ba781ed
Peter Zhen <peterzhen0713@gmail.com>
2016-10-04 11:39:05 -0400

    added patch to users controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -10,6 +10,16 @@ class Api::UsersController < ApplicationController
 		end
 	end
 
+  def update
+    @user = User.find_by_username(params[:username]);
+
+		if @user.update(user_params)
+			render "api/users/show"
+		else
+			render json: @user.errors.full_messages, status: 422
+		end
+  end
+
 	private
 
 	def user_params
