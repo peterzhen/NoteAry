b4ca3b1f519934a8be35de89d9d00ce7d5d9cee9
Peter Zhen <peterzhen0713@gmail.com>
2016-10-24 11:47:26 -0400

    change webpack

--- webpack.config.js
+++ webpack.config.js
@@ -22,17 +22,5 @@ module.exports = {
   devtool: 'source-maps',
   resolve: {
     extensions: ["", ".js", ".jsx" ]
-  },
-  plugins:[
-   new webpack.DefinePlugin({
-     'process.env':{
-       'NODE_ENV': JSON.stringify('production')
-     }
-   }),
-   new webpack.optimize.UglifyJsPlugin({
-     compress:{
-       warnings: true
-     }
-   })
- ]
+  }
 };
