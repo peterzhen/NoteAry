7cd9423df29a540db0b4cf6b3ec20d7762b35af6
Peter Zhen <peterzhen0713@gmail.com>
2016-10-24 11:43:44 -0400

    remove logger, change logo

--- frontend/middleware/root_middleware.js
+++ frontend/middleware/root_middleware.js
@@ -3,14 +3,12 @@ import SessionMiddleware from '../middleware/session_middleware';
 import NoteMiddleware from '../middleware/note_middleware';
 import NotebookMiddleware from '../middleware/notebook_middleware';
 import TagMiddleware from '../middleware/tag_middleware';
-import CreateLogger from 'redux-logger';
 
 const RootMiddleware = applyMiddleware(
   SessionMiddleware,
   NoteMiddleware,
   NotebookMiddleware,
-  TagMiddleware,
-  CreateLogger()
+  TagMiddleware
 );
 
 export default RootMiddleware;
--- webpack.config.js
+++ webpack.config.js
@@ -22,5 +22,17 @@ module.exports = {
   devtool: 'source-maps',
   resolve: {
     extensions: ["", ".js", ".jsx" ]
-  }
+  },
+  plugins:[
+   new webpack.DefinePlugin({
+     'process.env':{
+       'NODE_ENV': JSON.stringify('production')
+     }
+   }),
+   new webpack.optimize.UglifyJsPlugin({
+     compress:{
+       warnings: true
+     }
+   })
+ ]
 };
