1309141042462e30c89637eb28e3430a950e9c0d
Peter Zhen <peterzhen0713@gmail.com>
2016-10-24 11:55:56 -0400

    modify webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,10 @@
 // webpack.config.js
+new webpack.DefinePlugin({
+"process.env": {
+   NODE_ENV: JSON.stringify("production")
+ }
+})
+
 module.exports = {
   context: __dirname, // for rails
   entry: "./frontend/noteary.jsx",
